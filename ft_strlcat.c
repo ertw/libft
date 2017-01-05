518179c3b405e20c78beca96c1f5a5af4aeb7172
Erik Williamson <erik@himself.in>
2017-01-05 14:03:10 -0800

    broken but shorter strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -15,27 +15,49 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dlen;
-	size_t	available_buffer;
-	size_t	n;
+	size_t	slen;
 
-	n = size;
-	dlen = 0;
-	while (*dst && n--)
-	{
-		dlen++;
-		dst++;
-	}
-	available_buffer = size - dlen - 1;
-	if (*dst)
-		return (dlen + ft_strlen(src));
-	while (*src && available_buffer)
-	{
-		*dst++ = *src++;
-		dlen++;
-		available_buffer--;
-	}
-	*dst = '\0';
-	while (*src++)
-		dlen++;
-	return (dlen);
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	if (dlen >= size)
+		return (dlen + slen);
+	ft_strncat(dst, src, size - 1);
+	return (dlen + slen);
 }
+
+
+
+//	size_t	dlen;
+//	size_t	slen;
+//
+//	dlen = ft_strlen(dst);
+//	slen = ft_strlen(src);
+//	if (dlen == size)
+//		return (dlen + slen);
+//	ft_strncat(dst, src, size != dlen + slen + 1 ? dlen : size - dlen);
+//	return (dlen + slen);
+
+//	size_t	dlen;
+//	size_t	available_buffer;
+//	size_t	n;
+//
+//	n = size;
+//	dlen = 0;
+//	while (*dst && n--)
+//	{
+//		dlen++;
+//		dst++;
+//	}
+//	available_buffer = size - dlen - 1;
+//	if (*dst)
+//		return (dlen + ft_strlen(src));
+//	while (*src && available_buffer)
+//	{
+//		*dst++ = *src++;
+//		dlen++;
+//		available_buffer--;
+//	}
+//	*dst = '\0';
+//	while (*src++)
+//		dlen++;
+//	return (dlen);
