4ed47a15b04c876754944e94547071c8b3e80c3f
Erik Williamson <ewilliam@e1z3r5p19.42.us.org>
2017-03-09 10:48:44 -0800

    more safety improvements

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*node;
 
-	node = malloc(sizeof(t_list));
+	node = ft_memalloc(sizeof(t_list));
 	if (node)
 	{
 		if (!content)
@@ -26,7 +26,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		}
 		else
 		{
-			node->content = ft_memalloc(sizeof(size_t) * content_size);
+			node->content = ft_memalloc(content_size);
 			ft_memmove(node->content, content, content_size);
 		}
 		node->content_size = content_size;
